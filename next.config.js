2f3a776cf675c2c01fc4fbf1a52be18cf9a2f312
Ananda Gharami <86967865+gharamiananda@users.noreply.github.com>
2024-05-17 07:51:23 +0000

    about page

--- next.config.js
+++ next.config.js
@@ -1,24 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      webpack: (config, options) => {
-    // Customize webpack configuration here
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(node)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+    });
     return config;
   },
-  module: {
-    rules: [
-      // other rules...
-
+  async redirects() {
+    return [
       {
-        test: /\.(node)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]'
-          }
-        }
-      }
-    ]
-  }
-}
+        source: '/:path*',
+        has: [
+          {
+            type: 'query',
+            key: 'path',
+            value: '.+', // Matches any non-empty value
+          },
+        ],
+        permanent: true,
+        destination: ({ params, query }) => {
+          // Remove unwanted characters from the path
+          const cleanedPath = params.path.replace(/[%20%]/g, '').replace(/ /g, '');
+          return `/${cleanedPath}`;
+        },
+      },
+      {
+        source: '/:path*',
+        destination: ({ params }) => {
+          // General cleanup for any unwanted characters
+          const cleanedPath = params.path.replace(/[%20%]/g, '').replace(/ /g, '');
+          return `/${cleanedPath}`;
+        },
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
