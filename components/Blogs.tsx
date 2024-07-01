ceea3b1a188af493f04dfeae3489fba88666d4c1
Ananda Gharami <86967865+gharamiananda@users.noreply.github.com>
2024-07-01 09:59:41 +0530

    Update Blogs.tsx

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -1,22 +1,10 @@
 import { fetchMediumPosts } from '@/utils/fetchMediumPosts';
-import React from 'react';
 import BlogSlider from './BlogSlider';
 
 
 
-type Post = {
-  
-  title: string;
-  link: string;
-  guid: string;
-  pubDate:string;
-  contentSnippet:string;
-  author:string
-  };
-  
-  type Props = {
-    posts: Post[];
-  };
+
+
   
 
 
@@ -33,7 +21,7 @@ const posts:any=await fetchMediumPosts();
 
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-      <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">My Blog</h2>
+      <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-white dark:text-white">My Blog</h2>
       <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
     </div> 
 
